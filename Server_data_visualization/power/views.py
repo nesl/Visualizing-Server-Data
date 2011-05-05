929cd8024eb0a6a54ca4146d8f8e620490ab9904
Jason Tsao <jtsao22@gmail.com>
2011-05-04 19:27:14 -0700

    Everything is working now. Cleaned up some code

--- Server_data_visualization/power/views.py
+++ Server_data_visualization/power/views.py
@@ -10,42 +10,32 @@ import json
 
 # Set up connection with Mongodb
 connection = Connection('localhost', 90)
-db = connection.test_database3
+db = connection.visual_server_db
 
 def charts_menu(request):
     return render_to_response('power/charts_menu.html',
         context_instance=RequestContext(request))
 
 def results(request, field, field_val):
-    # print "POST: ",request.GET.get('channel')
     get_val = request.GET.get('channel')
     if get_val != None:
         field = "data_channel"
         field_val = int(get_val)
 
     data_list = {"field":field, "field_val":field_val}
-    print "DATA_LIST: ",data_list
     return render_to_response('power/detail.html', {'data_list': data_list})
 
 def posted_results(request):
-    # print "POST: ",request.GET.get('channel')
-    print "Getting results"
     req = request.GET
     for field, field_val in req.iteritems():
-        print "field: ", field
-        print "field_val: ", field_val
         if field != "type":
             data_list = {"field" :field, "field_val" : int(field_val)}
         else:
             if field_val.startswith("CPU") or field_val.startswith("RAM"):
                 data_list = {"field" :field, "field_val" : field_val}
-        print "Data list: ", data_list
     return render_to_response('power/detail.html', {'data_list': data_list})
 
 def get_data(request, field, field_val):
-    print "GETTING DATA"
-    print "GET: ",request.GET.get('sub')
-    data = []
     if field != "type":
         data_list = db.data.find({field: int(field_val)})
     else:
@@ -56,6 +46,7 @@ def get_data(request, field, field_val):
         else:
             data_list = db.data.find({field: field_val})
 
+    data = []
     for obj in data_list:
         data.append(obj)
     return HttpResponse(json.dumps(data, default=json_util.default))
