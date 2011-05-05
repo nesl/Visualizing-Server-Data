5a0dce1f830fe319f52441e47e478df6d7457279
Jason Tsao <jtsao22@gmail.com>
2011-05-05 15:38:29 -0700

    Adding to the database now updates according the daq0_description dynamically instead of hard-coded numbers. Therefore, changing the daq lines just requires changing the daq0_description and daq2_description now and will not need to be updated in code.

--- Server_data_visualization/add_to_database.py
+++ Server_data_visualization/add_to_database.py
@@ -4,50 +4,46 @@ from pymongo import Connection
 
 
 def add_to_database(daq):
+
+    # Connect to Mongodb
     connection = Connection('localhost', 90)
     connection.drop_database('visual_server_db')
     db = connection.visual_server_db
 
+    # Open the daq configuration file and daq file created from running the
+    # executable
+    if daq == 0:
+        daq_config = open("../cmd/config/daq0_description", "r")
+    else:
+        daq_config = open("../cmd/config/daq2_description", "r")
     daq_file = open("daq_results.txt", "r")
 
-    # Channel format specific to config/daq0_description
-    # and config/daq2_description
-    # Channel format is just a (node, device) pair if(daq == 0):
-    if daq == 0:
-        channel_format = [(0, "CPU0"),
-                          (0, "CPU1"),
-                          (0, "RAM0"),
-                          (0, "RAM1"),
-                          (-1, "CPU0"),
-                          (-1, "CPU1"),
-                          (-1, "RAM0"),
-                          (-1, "RAM1"),
-                          (4, "CPU0"),
-                          (4, "CPU1"),
-                          (4, "RAM0"),
-                          (4, "RAM1"),
-                          (1, "CPU0"),
-                          (1, "CPU1"),
-                          (1, "RAM0"),
-                          (1, "RAM1")]
-    else:
-        channel_format = [(3, "CPU0"),
-                          (3, "CPU1"),
-                          (3, "RAM0"),
-                          (3, "RAM1"),
-                          (6, "CPU0"),
-                          (6, "CPU1"),
-                          (6, "RAM0"),
-                          (6, "RAM1"),
-                          (2, "CPU0"),
-                          (2, "CPU1"),
-                          (2, "RAM0"),
-                          (2, "RAM1"),
-                          (5, "CPU0"),
-                          (5, "CPU1"),
-                          (5, "RAM0"),
-                          (5, "RAM1")]
 
+    # Read daq configuration file
+    config = daq_config.readline()
+    token_list = config.split(" ")
+
+    channel_format = [] # Channel format is a (node, device) list
+    counter = 0
+    daq_channel = []
+    for token in token_list:
+        daq_channel.append(token)
+        if counter >= 2 or token.count('"') == 2:
+            if token.count('"') != 2:
+                # Read daq channel line such as:
+                # "Node 0: CPU0"
+                node = daq_channel[1].replace(':', '')
+                peripheral = daq_channel[2].replace('",', '')
+                channel_format.append((int(node),peripheral))
+            else:
+                # Parse N/C channels
+                channel_format.append((-1, "N/C"))
+            daq_channel = []
+            counter = 0
+        else:
+            counter += 1
+
+    # Read the daq file and store the data into Mongodb
     for line in daq_file:
         tokened_line = line.split(' ')
         count = 0
@@ -67,3 +63,7 @@ def add_to_database(daq):
             if count == 15:
                 break
             count += 1
+
+    # Close the files
+    daq_file.close()
+    daq_config.close()
