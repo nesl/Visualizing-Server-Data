1e6d072b6284cf625421de8eae715b65592d7366
Jason Tsao <jtsao22@gmail.com>
2011-06-06 10:42:40 -0700

    Apache + Django deployment is currently working.

--- Server_data_visualization/views.py
+++ Server_data_visualization/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.context_processors import csrf
 from django import forms
+from django.template import RequestContext
 
 # Subprocess, to spawn new process and return output
-import subprocess, shlex
+import subprocess, shlex, signal
 import settings
 import stat
 import time
@@ -21,7 +22,8 @@ class UploadFileForm(forms.Form):
 
 def handle_uploaded_file(f):
     """ Writes the file in chunks to the file system with RWX privileges """
-    path = settings.ABS_PATH + "Server_data_visualization/uploads/" + f.name
+    # path = settings.ABS_PATH + "Server_data_visualization/uploads/" + f.name
+    path = settings.ABS_PATH + "Server_data_visualization/uploads/executable"
     destination = open(path, "wb+")
     for chunk in f.chunks():
         destination.write(chunk)
@@ -43,7 +45,7 @@ def upload_file(request):
             # os.setuid(0)
 
             # Start the DAQ and get PID
-            cmd = settings.ABS_PATH + "cmd/daq daq0"
+            cmd = "sudo " + settings.ABS_PATH + "cmd/daq daq0"
             args = shlex.split(cmd)
             print "ARGS: ", args
             process = subprocess.Popen(args,\
@@ -52,7 +54,8 @@ def upload_file(request):
             # Run the executable
             parameters = request.POST['parameters']
             args = "time " + settings.ABS_PATH +\
-            "Server_data_visualization/uploads/" + file_name + " " + parameters
+            "Server_data_visualization/uploads/executable " + parameters
+            #"Server_data_visualization/uploads/" + file_name + " " + parameters
             results = subprocess.Popen((args),\
                     stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True).communicate()
 
@@ -61,14 +64,18 @@ def upload_file(request):
 
             # Stop the DAQ and get the DAQ results
             print "STOPPING PID: ", process.pid
-            os.system("kill %s" % (process.pid))
+            os.system("sudo kill -9 %s" % (process.pid))
+            #process.kill()
             daq_results = process.communicate()
 
             print "DAQ_RESULTS: " + daq_results[0]
             print "\n"
+            print "DAQ_ERR: " + daq_results[1]
+            print "\n"
 
             # If no errors or the comedi device is not busy
             if results[1] == None or daq_results[1].find("busy") == -1:
+                print "GOT IN HERE"
                 # Save the DAQ results to output file for reference later
                 f = open(settings.ABS_PATH + "Server_data_visualization/daq_results.txt", "w")
                 f.write(daq_results[0])
@@ -93,25 +100,31 @@ def upload_file(request):
             return render_to_response("upload_success.html", {'c': c})
 
         else:
-            # Ask for the upload again if the form is not valid
-            if password != "asdf":
-                return render_to_response("signin_failure.html")
+            c = RequestContext(request)
 
+            # Get username and password and check
+            username = request.POST["username"]
+            password = request.POST["password"]
+            if username != "admin" or password != "password":
+                return render_to_response("signin_failure.html",c)
+
+            # Ask for the upload again if the form is not valid
             form = UploadFileForm()
-            c = {}
-            c.update(csrf(request))
             c["form"] = form
 
             return render_to_response("upload.html", c)
 
     else:
-        # Create the form for uploading the file
-        if password != "asdf":
-            return render_to_response("signin_failure.html")
+        c = RequestContext(request)
 
+        # Get username and password and check
+        username = request.POST["username"]
+        password = request.POST["password"]
+        if username != "admin" or password != "password":
+            return render_to_response("signin_failure.html",c)
+
+        # Create the form for uploading the file
         form = UploadFileForm()
-        c = {}
-        c.update(csrf(request))
         c["form"] = form
 
         return render_to_response("upload.html", c)
